0d1ec0e2d3716d5c06cfda76b9a6d8b52ab6a875
guligula <imanishikazuki@konseiikki-no-MacBook-Pro.local>
2016-07-09 14:57:26 +0900

    テストメインを作ってみた

---
+++ Shirodora/Classes/Scene/Main/testMain.cpp
@@ -0,0 +1,13 @@
+//
+//  testMain.cpp
+//  Shirodora
+//
+//  Created by 今西一喜 on 2016/07/09.
+//
+//
+
+#include "testMain.hpp"
+
+
+
+//テストだよ
---
+++ Shirodora/Classes/Scene/Main/testMain.hpp
@@ -0,0 +1,14 @@
+//
+//  testMain.hpp
+//  Shirodora
+//
+//  Created by 今西一喜 on 2016/07/09.
+//
+//
+
+#ifndef testMain_hpp
+#define testMain_hpp
+
+#include <stdio.h>
+
+#endif /* testMain_hpp */
